b0b430afef3015bb2ed335469005d1ebe2011d3e
Jan Reimes <jan.reimes@head-acoustics.com>
2021-06-23 17:56:48 +0200

    Improved calculation of P.863

--- tests/test_polqa.py
+++ tests/test_polqa.py
@@ -13,7 +13,7 @@ class P863CalcTestCase(unittest.TestCase):
         # analysis parameters for the ETSI test files:
         duration = 8.0
         startTime = 16.0
-        nbrRanges = 2
+        nbrRanges = 8
 
         # init storage and new column
         df = pandas.read_excel(resultsP863File, index_col=0)
@@ -21,22 +21,24 @@ class P863CalcTestCase(unittest.TestCase):
             df['MOS-LQO'] = pandas.NA
 
         # calculate POLQA scores
-        for key, row in df.iterrows():
-            print(key)
-            if not ('MOS-LQO' in df.columns) or pandas.isna(df.loc[key, 'MOS-LQO']):
+        for i, (key, row) in enumerate(df.iterrows()):
+            print("[%d/%d] %s" % (i+1, df.shape[0], Path(key).name))
+            if pandas.isna(df.loc[key, 'MOS-LQO']):
                 dfPerFile = None
                 for i in range(nbrRanges):
                     res, _ = runPOLQA(key, key, chNbrRef=2, chNbrDeg=1,
                                    timeRangeStart=startTime + i*duration,
                                    timeRangeDuration=duration)
-                    if dfPerFile is None:
-                        dfPerFile = pandas.DataFrame(columns=res.index)
+                    if res.shape[0] > 0:
+                        if dfPerFile is None:
+                            dfPerFile = pandas.DataFrame(columns=res.index)
 
-                    dfPerFile.loc[i, :] = res
+                        dfPerFile.loc[i, :] = res
 
                 # store in output
-                df.loc[key, 'MOS-LQO'] = dfPerFile['MOS-LQO'].mean()
-                df.to_excel(resultsP863File)
+                if not dfPerFile is None:
+                    df.loc[key, 'MOS-LQO'] = dfPerFile['MOS-LQO'].mean()
+                    df.to_excel(resultsP863File)
 
 
 if __name__ == '__main__':
