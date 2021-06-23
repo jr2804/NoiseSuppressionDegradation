18bc33fdc6b0dc680be0ae29b96492b8d964fbba
Jan Reimes <jan.reimes@head-acoustics.com>
2021-06-23 17:56:09 +0200

    Added support for multiple input files

--- tests/test_degradeSpecSub.py
+++ tests/test_degradeSpecSub.py
@@ -13,40 +13,45 @@ class SpecSubDegradeTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.outputPath = thisPath / 'output'
-        for wvFile in cls.outputPath.glob('*.wav'):
-            wvFile.unlink()
+        #for wvFile in cls.outputPath.glob('*.wav'):
+        #    wvFile.unlink()
         cls.outputPath.mkdir(exist_ok=True)
-        cls.testFile = downloadETSITestFile(TestFilesETSI.German, proxy="https://127.0.0.1:3128")
+        cls.testFile = None
 
     def test_specsub(self):
         # storage for generated files
         if resultsP863File.is_file():
             df = pandas.read_excel(resultsP863File, index_col='Filename')
         else:
-            df = pandas.DataFrame(columns=['Filename', 'SNR', 'OSF', 'TimeConst', 'PowExp']).set_index('Filename')
+            df = pandas.DataFrame(columns=['Filename', 'Language', 'SNR', 'OSF', 'TimeConst', 'PowExp']).set_index('Filename')
 
-        # load signal
-        fs = librosa.get_samplerate(self.testFile)
-        s, _ = librosa.load(self.testFile, fs)
+        # run for all test signals
+        for tstFile in TestFilesETSI:
+            with self.subTest(testFile=tstFile.value):
+                self.testFile = downloadETSITestFile(tstFile, proxy="https://127.0.0.1:3128")
 
-        # iterate over internal pseudo-noise-reduction parameters:
-        for snr in [10, 0, -10, -20, -30]: # SNR between speech and speech-shaped noise
-            for osf in [0.5, 1.0, 1.5, 2.0]: # over-subtraction factor
-                for tc in [0.005, 0.035, 0.125, 0.250]: # time constant for smoothing
-                    for pow_exp in [0.5, 1.0, np.sqrt(2), 2.0]: # power exponent for Wiener gain
-                        with self.subTest(snr=snr, osf=osf, tc=tc, pow_exp=pow_exp):
-                            # run with settings
-                            outputFile = self.outputPath / Path('processed_snr=%d_osf=%.2f_tc=%d_pe=%.2f.wav' % (snr, osf, tc*1000, pow_exp))
-                            if not outputFile.is_file():
-                                d = applySpecSub(s, fs, snr=snr, osf=osf, tcNoise=tc, tcSpeech=tc)
+                # load signal
+                fs = librosa.get_samplerate(self.testFile)
+                s, _ = librosa.load(self.testFile, fs)
 
-                                # store degraded and reference in one file
-                                signal = np.vstack((d, s)).T
-                                sf.write(outputFile, signal, fs)
+                # iterate over internal pseudo-noise-reduction parameters:
+                for snr in [10, 0, -10, -20, -30]: # SNR between speech and speech-shaped noise
+                    for osf in [0.5, 1.0, 1.5, 2.0]: # over-subtraction factor
+                        for tc in [0.005, 0.035, 0.125, 0.250]: # time constant for smoothing
+                            for pow_exp in [0.5, 1.0, np.sqrt(2), 2.0]: # power exponent for Wiener gain
+                                with self.subTest(snr=snr, osf=osf, tc=tc, pow_exp=pow_exp):
+                                    # run with settings
+                                    outputFile = self.outputPath / Path('processed_%s_snr=%d_osf=%.2f_tc=%d_pe=%.2f.wav' % (tstFile.value, snr, osf, tc*1000, pow_exp))
+                                    if not outputFile.is_file():
+                                        d = applySpecSub(s, fs, snr=snr, osf=osf, tcNoise=tc, tcSpeech=tc)
 
-                            # store information for P.863 calculation in other unit test
-                            df.loc[str(outputFile), :] = [snr, osf, tc, pow_exp]
-                            df.to_excel(resultsP863File)
+                                        # store degraded and reference in one file
+                                        signal = np.vstack((d, s)).T
+                                        sf.write(outputFile, signal, fs)
+
+                                    # store information for P.863 calculation in other unit test
+                                    df.loc[str(outputFile), :] = [tstFile.value, snr, osf, tc, pow_exp]
+                                    df.to_excel(resultsP863File)
 
 if __name__ == '__main__':
     unittest.main()
