14b8cce349f856dcd1d3b1ef05ec6e2f47070ea8
Jan Reimes <jan.reimes@head-acoustics.com>
2021-06-25 16:01:48 +0200

    Improvement regarding speech level: - (active) speech level must be passed into degradation, to adequately calculate internal SNR - LTASS can be scaled to a certain level

--- degradeSpecSub.py
+++ degradeSpecSub.py
@@ -10,7 +10,7 @@ import librosa
 from scipy.signal import lfilter
 from scipy.interpolate import interp1d
 
-def ltasP50(freq, freq_lower=None, freq_upper=None, fmin=100, fmax=8000):
+def ltasP50(freq, freq_lower=None, freq_upper=None, fmin=100, fmax=8000, targetLevelDbPa=-4.7):
     freq = np.maximum(freq, 1.0) # avoid f=0
     if freq_lower is None:
         freq_lower = freq[0]
@@ -31,9 +31,13 @@ def ltasP50(freq, freq_lower=None, freq_upper=None, fmin=100, fmax=8000):
     delta_f = freq_upper - freq_lower
     corr_f = 10*np.log10(delta_f)
     S = Sd_i + corr_f
-    return S
 
-def applySpecSub(signal, fs, snr, **kwargs):
+    # scale to target level
+    levelDbPa = 10 * np.log10(np.sum(np.power(10, S / 10) / freq.shape[0]))
+    diff = targetLevelDbPa - levelDbPa
+    return S + diff
+
+def applySpecSub(signal, fs, speechLevel, snr, **kwargs):
     # parse arguments
     overlap = kwargs.get('overlap', 0.50)
     n_fft = kwargs.get('n_fft', 1024)
@@ -57,23 +61,17 @@ def applySpecSub(signal, fs, snr, **kwargs):
     freq = librosa.fft_frequencies(fs, n_fft)
     stft_args = dict(n_fft=n_fft, win_length=n_fft, hop_length=hop_length, window=window, center=True)
     S = librosa.stft(signal, **stft_args)
-    S_avg = np.abs(S.mean(axis=1))
-    levelS = 10*np.log10(np.sum(S_avg**2) / n_fft)
 
-    # speech-shaped noise
+    # generate white noise at 0 dB
     n = np.random.randn(signal.shape[0]).astype(np.float32)
     N = librosa.stft(n, **stft_args)
 
-    S_ltass = ltasP50(freq)
+    # generate speech-shaped noise at target level
+    targetNoiseLevel = speechLevel - snr
+    S_ltass = ltasP50(freq, targetLevelDbPa=targetNoiseLevel)
+
     S_ltass = np.power(10, S_ltass/20)
-
     N *= np.repeat(np.reshape(S_ltass, (S_ltass.shape[0],1)), N.shape[1], axis=1)
-    levelN = 10*np.log10(np.sum(np.abs(N.mean(axis=1))**2) / n_fft)
-
-    # scale noise
-    diff = (levelS - levelN) - snr
-    levelN += diff
-    N *= np.power(10, diff/20)
 
     # combine!
     Y = S + N
@@ -84,18 +82,22 @@ def applySpecSub(signal, fs, snr, **kwargs):
     absY = lfilter([1-aS], [1, -aS], np.abs(Y), axis=1)
     absN = lfilter([1-aN], [1, -aN], np.abs(N), axis=1)
 
-    # spectral subtraction
+    # spectral subtraction, taking into account over-subtraction and minimum noise floor
     S_est = np.maximum(absY-osf*absN, floorSubtractFactor*absY)
 
     # Wiener gain
     G = np.power(S_est**pow_exp/(S_est**pow_exp + absN**pow_exp), 1/pow_exp)
 
+    # Processed signal/STFT
+    P = S * G
+
     # transform back to time domain
     stft_args.pop('n_fft')
-    degraded = librosa.istft(S*G, **stft_args)
+    degraded = librosa.istft(P, **stft_args)
 
     # zero padding
     degraded = np.pad(degraded, (0, signal.shape[0]-degraded.shape[0])).astype(np.float32)
+
     return degraded
 
 if __name__ == "__main__":
